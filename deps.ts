81df5b9a7d7cec664a030568f1fe7bcf6059e5c6
mstoecklein <mstoecklein@xp.live>
2023-01-13 01:51:33 +0100

    window

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 // Workaround to get this to work with service workers
-// deno-lint-ignore no-var no-unused-vars
-var window = globalThis;
+// deno-lint-ignore no-explicit-any
+(globalThis as any).window = globalThis;
 
 export * from "https://deno.land/x/offgrid_engine@v1.0.0/main.ts";
 export { default as Handlebars } from "https://unpkg.com/handlebars@4.7.7/lib/handlebars.js";
