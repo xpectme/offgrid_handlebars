cf1edad195dc22364a34b4a9d15813a7d193504a
mstoecklein <mstoecklein@xp.live>
2023-01-13 02:31:38 +0100

    unpkg.com -> esm.sh

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export * from "https://deno.land/x/offgrid_engine@v1.0.1/main.ts";
-export { default as Handlebars } from "https://unpkg.com/handlebars@4.7.7/lib/handlebars.js";
+export { default as Handlebars } from "https://esm.sh/v102/handlebars@4.7.7/es2022/handlebars.js";
